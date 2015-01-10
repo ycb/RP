7909fcefdfddc87e38f56f4164981a55989706bf
YCB <info@yourcustomblog.com>
2015-01-10 15:10:16 -0800

    favicon

--- wp-content/themes/aaf/header.php
+++ wp-content/themes/aaf/header.php
@@ -9,6 +9,7 @@
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.gif" />
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -75,7 +76,7 @@
                 <input type="submit" class="submit btn fsSubmitButton" name="submit" id="searchsubmit" value="GO">
             </form>
         </div>
-        <div class="span3 donate"><a href="<?php bloginfo('url'); ?>/?page_id=23">Donate!</a></div><div class="span3 news"><a href="<?php bloginfo('url'); ?>/sign-up/">Get Involved</a></div>
+        <div class="span3 donate"><a href="<?php bloginfo('url'); ?>/?page_id=23">Donate</a></div><div class="span3 news"><a href="<?php bloginfo('url'); ?>/sign-up/">Get Involved!</a></div>
     </div>  
     <div id="page" class="hfeed site">
         <?php do_action( 'before' ); ?>
